cc5b3f7e1e64db51170143050d717fe9056091a7
WOWAbid <hasan2007107@gmail.com>
2024-11-24 00:00:08 +0600

    shop listing ui & product fetch done

--- server/models/User.js
+++ server/models/User.js
@@ -8,27 +8,37 @@ const UserSchema = new mongoose.Schema({
   },
   userName: {
     type: String,
-    required: function() {
-      return !this.googleId; 
+    required: function () {
+      return !this.googleId;
+    },
+    default: function () {
+      return this.googleId ? `google_user_${Date.now()}` : undefined;
     },
     unique: true,
+    sparse: true, 
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    validate: {
+      validator: function (v) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v); // Basic email regex
+      },
+      message: (props) => `${props.value} is not a valid email address!`,
+    },
   },
   password: {
     type: String,
-    required: function() {
-      return !this.googleId; 
+    required: function () {
+      return !this.googleId;
     },
   },
   role: {
     type: String,
     default: "user",
-  }
-  //profileImage: String, 
+  },
+  // profileImage: String,
 });
 
 const User = mongoose.model("User", UserSchema);
