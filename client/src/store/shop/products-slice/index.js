f26e7e4c97fd997048bdf25c5dd661809fa314e9
Khandoker Abid Hasan <hasan2007107@stud.kuet.ac.bd>
2024-11-25 22:57:22 +0600

    FIlter & Details Done !!!

--- client/src/store/shop/products-slice/index.js
+++ client/src/store/shop/products-slice/index.js
@@ -1,19 +1,37 @@
-//const { createSlice } = require("@reduxjs/toolkit");
-import { Action } from "@radix-ui/react-toast";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-    isLoading : false,
-    productList : [],
-}
-
+  isLoading: false,
+  productList: [],
+  productDetails: null,
+};
 
 export const fetchAllFilteredProducts = createAsyncThunk(
   "/products/fetchAllProducts",
-  async () => {
+  async ({ filterParams, sortParams }) => {
+    console.log(fetchAllFilteredProducts, "fetchAllFilteredProducts");
+
+    const query = new URLSearchParams({
+      ...filterParams,
+      sortBy: sortParams,
+    });
+
     const result = await axios.get(
-      "http://localhost:5000/api/shop/products/get"
+      `http://localhost:5000/api/shop/products/get?${query}`
+    );
+
+    console.log(result);
+
+    return result?.data;
+  }
+);
+
+export const fetchProductDetails = createAsyncThunk(
+  "/products/fetchProductDetails",
+  async (id) => {
+    const result = await axios.get(
+      `http://localhost:5000/api/shop/products/get/${id}`
     );
 
     return result?.data;
@@ -21,26 +39,40 @@ export const fetchAllFilteredProducts = createAsyncThunk(
 );
 
 const shoppingProductSlice = createSlice({
-    name : "shoppingProducts",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-      builder.addCase(fetchAllFilteredProducts.pending,(state,action)=>{
-        state.isLoading=true;
-
-      }).addCase(fetchAllFilteredProducts.fulfilled,(state,action)=>{
-        console.log(action.payload, "action.payload");
-        state.isLoading=false;
-        state.productList=action.payload.data;
-      }).addCase(fetchAllFilteredProducts.rejected,(state,action)=>{
-        console.log(action.payload);
-        state.isLoading=false;
-        state.productList=[];
+  name: "shoppingProducts",
+  initialState,
+  reducers: {
+    setProductDetails: (state) => {
+      state.productDetails = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllFilteredProducts.pending, (state, action) => {
+        state.isLoading = true;
       })
-    }
-
+      .addCase(fetchAllFilteredProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.productList = action.payload.data;
+      })
+      .addCase(fetchAllFilteredProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.productList = [];
+      })
+      .addCase(fetchProductDetails.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchProductDetails.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.productDetails = action.payload.data;
+      })
+      .addCase(fetchProductDetails.rejected, (state, action) => {
+        state.isLoading = false;
+        state.productDetails = null;
+      });
+  },
 });
 
+export const { setProductDetails } = shoppingProductSlice.actions;
 
-  
-  export default shoppingProductSlice.reducer;
+export default shoppingProductSlice.reducer;
